0abbf4ce1c68bb468b09015c26290e1d99340af4
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2024-11-25 09:18:47 +0800

    Update RemoteConfigs.ts

--- src/config/RemoteConfigs.ts
+++ src/config/RemoteConfigs.ts
@@ -5,6 +5,10 @@ type RemoteConfig = {
 
 export const RemoteConfigs: RemoteConfig[] = [
     {
+        key: "DNS防泄漏",
+        value: "https://raw.githubusercontent.com/CitizenScyu/clash-rules/refs/heads/master/DNS.ini"
+    },
+    {
         key: "ACL_默认版",
         value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
     },
